5da13eda74f669b1c06371eb45bc7337a6020380
imahasanur <imahasanur@gmail.com>
2021-01-22 16:56:07 +0600

    added budgetCalculator function and edited kilometerToMeter function

--- assignment.js
+++ assignment.js
@@ -1,11 +1,30 @@
 //
 // kilometer to meter convertion
 function kilometerToMeter(kiloMeter){
-  if (kiloMeter < 0){
-    return "measurement can not be negative, enter a positive value";
+  if(kiloMeter < 0){
+    return "Measurement can not be negative, enter a positive value";
+  }
+  else if(!(typeof kiloMeter == "number")){
+      return "Enter a number";
   }
   meter = kiloMeter *1000; // 1 km = 1000 meter;
   meter = Number(meter.toFixed(2));
-  return  typeof meter;
+  return meter;
 }
+// console.log(kilometerToMeter(null))
 
+//Caluculation of total amount of products
+function budgetCalculator(ghori, phone, laptop){
+  if(ghori < 0 || phone < 0 || laptop < 0){
+    return "Amount can not be negative , enter a valid number";
+  }
+  else if(!(typeof ghori == "number") || !(typeof phone == "number") || !(typeof laptop == "number")){
+    return "Enter a number";
+  }
+  else if((ghori%1 > 0) || (phone%1 > 0) || (laptop%1 > 0)){
+    return "Enter a integer number of product";
+  }
+  var totalCost = (ghori*50) + (phone*100) + (laptop*500);
+  return totalCost;
+}
+// console.log(budgetCalculator(3,.35,2))
